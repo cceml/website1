64800bacbfd00d19a787107b39796f737a9f5d44
cem hüseyin lafçı <yoo_xd05@hotmail.com>
2022-05-26 16:58:13 +0300

    javascript validation

--- js/extra.js
+++ js/extra.js
@@ -29,3 +29,33 @@ for (let i = 0; i <BtnCanvas.length;i++){
     });
 }
 // navbar javascript
+(function(){
+    'use strict'
+    var myName=document.querySelector("#name");
+    var myEmail=document.querySelector("#email");
+    var myPhone=document.querySelector("#phone");
+    var myMessage=document.querySelector("#message");
+    var myBtn=document.querySelector("#BtnContact");
+
+    if(myMessage.value.length == 0) {
+        myBtn.disabled=true;
+    }
+    myMessage.addEventListener("keyup",function(){
+        document.getElementById("current-character").textContent=myMessage.value.length;
+
+        if (myMessage.value.length >= 10){
+            myBtn.disabled=false;
+        }
+        else {
+            myBtn.disabled = true;
+        }
+    });
+    var myForms=document.querySelector(".needs-validation");
+    myForms.addEventListener("submit",function(e){
+        if(!myForms.checkValidity()){
+            e.preventDefault();
+            e.stopPropagation();
+        }
+    },false);
+    
+})();
